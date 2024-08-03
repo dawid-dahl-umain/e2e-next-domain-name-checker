300790d8284f1d749dc4d18d34a7b07607a1ade9
Dawid Dahl <dawid.dahl@umain.com>
2024-08-03 17:29:04 +0200

    chore: Add Jest setup file and update Jest configuration

--- app/components/DomainAvailabilityChecker.spec.tsx
+++ app/components/DomainAvailabilityChecker.spec.tsx
@@ -1,9 +1,21 @@
 import React from "react"
-import { render } from "@testing-library/react"
+import { fireEvent, render, screen } from "@testing-library/react"
 import { DomainAvailabilityChecker } from "./DomainAvailabilityChecker"
 
 describe("DomainAvailabilityChecker", () => {
     it("should display domain name when specified by user", () => {
         render(<DomainAvailabilityChecker />)
+
+        const inputField = screen.getByRole("textbox")
+
+        fireEvent.change(inputField, { target: { value: "www.tddrocks.com" } })
+
+        const button = screen.getByRole("button")
+
+        fireEvent.click(button)
+
+        expect(
+            screen.getByText("The searched domain is: www.tddrocks.com")
+        ).toBeInTheDocument()
     })
 })
